471f64e84e3640fd013f814c8266a1aa6dea4346
EC2 Default User <ec2-user@ip-172-31-11-36.ap-northeast-1.compute.internal>
2025-06-24 11:32:06 +0000

    listenのコメントアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_one_attached :profile_image
   has_many :favorites, dependent: :destroy
+  has_many :favorited_posts, through: :favorites, source: :post
   
   # 自分がフォローされる（被フォロー）側の関係性
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
