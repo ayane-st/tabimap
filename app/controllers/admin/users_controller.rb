eef8d183a9ca41b69b5c071e68ee235b286e6083
EC2 Default User <ec2-user@ip-172-31-11-36.ap-northeast-1.compute.internal>
2025-06-24 12:53:39 +0000

    adminの修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,4 +5,9 @@ class Admin::UsersController < ApplicationController
     @user.destroy
     redirect_to admin_dashboards_path,notice: 'ユーザーを削除しました'
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     get 'dashboards', to: 'dashboards#index'
-    resources :users, only: [:destroy]
+    resources :users, only: [:destroy, :show ]
   end
 
   devise_for :users, path: "/", controllers: {
